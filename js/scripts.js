98195b8034fa84798710a7c295d164d7369f7fa3
epicodus <student@epicodus.com>
2014-02-21 11:19:50 -0800

    We made a for loop and an object stuff

--- js/scripts.js
+++ js/scripts.js
@@ -9,55 +9,80 @@ var scoreForWord = function(word) {
 }
 
 var scoreForLetter = function(letter) {
-  
-  var letterLowerCase = letter.toLowerCase();
-  var onePointLetters = ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"];
-  var twoPointLetters = ["d", "g"];
-  var threePointLetters = ["b", "c", "m", "p"];
-  var fourPointLetters = ["f", "h", "v", "w", "y"];
-  var fivePointLetter = ["k"];
-  var eightPointLetters = ["j", "x"];
-  var tenPointLetters = ["q", "z"];
 
-  for (var i = 0; i < onePointLetters.length; i++) {
-    if (letterLowerCase === onePointLetters[i]) {
-      return 1;
-    }
-  }
+  var letterLowerCase = letter.toLowerCase();  
+  var letterScore = {
+    0:[1,2,3,4,5,8,10],
+    1:["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"], 
+    2:["d", "g"],
+    3:["b", "c", "m", "p"],
+    4:["f", "h", "v", "w", "y"],
+    5:["k"],
+    6:["j", "x"],
+    7:["q", "z"]
+  };
 
-  for (var j = 0; j < twoPointLetters.length; j++) {
-    if (letterLowerCase === twoPointLetters[j]) {
-      return 2;
+  for (var j = 1; j < 8; j++) {
+    for (var i = 0; i < letterScore[j].length; i++) {
+      if (letterScore[j][i] === letterLowerCase) {
+        return letterScore[0][j-1];
+      }
     }
-  }
-
-  for (var k = 0; k < threePointLetters.length; k++) {
-    if (letterLowerCase === threePointLetters[k]) {
-      return 3;
-    }
-  }
-
-  for (var k = 0; k < fourPointLetters.length; k++) {
-    if (letterLowerCase === fourPointLetters[k]) {
-      return 4;
-    }
-  }
-
-  for (var i = 0; i < fivePointLetter.length; i++) {
-    if (letterLowerCase === fivePointLetter[i]) {
-      return 5;
-    }
-  }
-
-  for (var i = 0; i < eightPointLetters.length; i++) {
-    if (letterLowerCase === eightPointLetters[i]) {
-      return 8;
-    }
-  }
-
-  for (var i = 0; i < tenPointLetters.length; i++) {
-    if (letterLowerCase === tenPointLetters[i]) {
-      return 10;
-    }
-  }
+  };
 };
+
+
+
+
+
+//   var letterLowerCase = letter.toLowerCase();
+//   var onePointLetters = ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"];
+//   var twoPointLetters = ["d", "g"];
+//   var threePointLetters = ["b", "c", "m", "p"];
+//   var fourPointLetters = ["f", "h", "v", "w", "y"];
+//   var fivePointLetter = ["k"];
+//   var eightPointLetters = ["j", "x"];
+//   var tenPointLetters = ["q", "z"];
+
+//   for (var i = 0; i < onePointLetters.length; i++) {
+//     if (letterLowerCase === onePointLetters[i]) {
+//       return 1;
+//     }
+//   }
+
+//   for (var j = 0; j < twoPointLetters.length; j++) {
+//     if (letterLowerCase === twoPointLetters[j]) {
+//       return 2;
+//     }
+//   }
+
+//   for (var k = 0; k < threePointLetters.length; k++) {
+//     if (letterLowerCase === threePointLetters[k]) {
+//       return 3;
+//     }
+//   }
+
+//   for (var k = 0; k < fourPointLetters.length; k++) {
+//     if (letterLowerCase === fourPointLetters[k]) {
+//       return 4;
+//     }
+//   }
+
+//   for (var i = 0; i < fivePointLetter.length; i++) {
+//     if (letterLowerCase === fivePointLetter[i]) {
+//       return 5;
+//     }
+//   }
+
+//   for (var i = 0; i < eightPointLetters.length; i++) {
+//     if (letterLowerCase === eightPointLetters[i]) {
+//       return 8;
+//     }
+//   }
+
+//   for (var i = 0; i < tenPointLetters.length; i++) {
+//     if (letterLowerCase === tenPointLetters[i]) {
+//       return 10;
+//     }
+//   }
+// };
